c5ab66b7dabfbd53cb93a8a57ef8c557950af82e
meguko <meguko2002@gmail.com>
2022-02-10 23:44:38 +0900

    https://github.com/qwert45hi/flask-socketio-example.git　に倣ってみた

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO,emit
 
 app = Flask(__name__)
-# app.secret_key = 'ABCDEFGH'
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True
-
-socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route('/')
@@ -15,15 +11,10 @@ def index():
     return render_template('index.html')
 
 
-@socketio.on("message", namespace='/test')
+@socketio.on("message")
 def handleMessage(data):
     emit("new_message", data, broadcast=True)
 
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
 
-
-if __name__ == '__main__':
-    # socketio.run(app, host='0.0.0.0', port=5000, debug=True)
-    socketio.run(app)
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
